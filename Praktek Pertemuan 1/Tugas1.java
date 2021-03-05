8190be8047633bbe8052d42d56779166fa05d709
Firdanaac <111201911671@mhs.dinus.ac.id>
2021-03-05 14:49:22 +0000

    add Tugas1.java

---
+++ Praktek Pertemuan 1/Tugas1.java
@@ -0,0 +1,16 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+    int Soal_1 = -5 + 8 *6;
+    System.out.println( Soal_1);
+
+    int Soal_2= (55+9)%9;
+    System.out.println(Soal_2);
+
+    int Soal_3 = 20 + -3*5 /8;
+    System.out.println(Soal_3);
+
+    int soal_4 =  5 + 15 / 3 * 2 - 8 % 3;
+    System.out.println(soal_4);
+    }
+   }
+   
